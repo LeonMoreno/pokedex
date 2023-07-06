c9ac2247324751bed1b51872f909a64b5a64de0c
Leonardo Moreno <lmoreno@petalmd.com>
2023-07-05 20:59:48 -0400

    Update test factories -- unique LoL

--- test/factories/pokemons.rb
+++ test/factories/pokemons.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :pokemon do
     sequence(:id)
-    name { Faker::Games::Pokemon.name }
+    name { Faker::Games::Pokemon.unique.name }
     type_1 { "prue_1" }
     total { Faker::Number.number(digits: 3) }
     attack { Faker::Number.number(digits: 2) }
